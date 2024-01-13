fc5af52e0b7804656a64e5ad2950c85be21dbb26
copecone <controld3785@gmail.com>
2024-01-13 11:21:51 +0900

    Load Client Instance

--- client/src/main/kotlin/io/github/forceload/overlayer/OverlayerMod.kt
+++ client/src/main/kotlin/io/github/forceload/overlayer/OverlayerMod.kt
@@ -4,14 +4,18 @@ import com.mojang.logging.LogUtils
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.loader.api.FabricLoader
+import net.minecraft.client.MinecraftClient
 
 object OverlayerMod: ModInitializer {
-    const val MOD_ID = "sample"
+    const val MOD_ID = "overlayer"
     private lateinit var loaderInstance: FabricLoader
+    lateinit var clientInstance: MinecraftClient
+
     val logger = LogUtils.getLogger()
 
     override fun onInitialize() {
         loaderInstance = FabricLoader.getInstance()
+        clientInstance = MinecraftClient.getInstance()
 
         when (loaderInstance.environmentType) {
             EnvType.CLIENT -> {
